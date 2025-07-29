ec73af8d9e1f7cbf94fb1ab54463a1353bc786b4
avnish404 <91267441+avnish404@users.noreply.github.com>
2025-07-29 20:32:01 +0530

    Auth Service Creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import authService from './Appwrite/auth' 
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(()=>
+  {
+        authService.createAccount();
+  },[])
+  
 
   return (
     <>
---
+++ src/Appwrite/auth.js
@@ -0,0 +1,76 @@
+import { Client, Account, ID } from "appwrite";
+import conf from "./conf";
+
+
+class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client.setEndpoint(conf.appwriteUrl).setProject(conf.appwriteProjectId)
+
+        this.account = new Account(this.client)
+    }
+
+    async createAccount({email,password,name}) {
+
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+
+            if (userAccount) {
+                //Calling login method
+                console.log('Account Created', userAccount);
+                return this.login({email,password})
+            }
+
+            else {
+                console.log('Account creattion failed')
+                return userAccount;
+            }
+        } catch (error) {
+            console.error(error)
+
+            throw (error)
+        }
+
+
+
+    }
+
+    async login({email,password})
+    {
+        try {
+            console.log('login Successful')
+            return await this.account.createEmailPasswordSession(email,password)
+        } catch (error) {
+            console.error('failed to login',error)
+            throw(error)
+        }
+    }
+
+    async getCurrentUser(){
+    try {
+        return await this.account.get()
+    } catch (error) {
+        console.log('Appwrite error::',error)
+    }
+}
+
+
+        async logout(){
+            try {
+                return await this.account.deleteSessions();
+            } catch (error) {
+                
+                    console.log('Appwrite Error:: Logout :',error)
+                
+            }
+        }
+
+}
+
+
+
+const authService = new AuthService()
+
+export default authService;
