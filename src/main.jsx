81478c68fb7107a90fda08bc28e009293655d067
avnish404 <91267441+avnish404@users.noreply.github.com>
2025-08-25 21:42:43 +0530

    Router Setting enhanced

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./Home.jsx";
-import Signup from "./signup.jsx"; 
-import Login from "./Login.jsx";
+import Home from "./Pages/Home.jsx";
+import Signup from "./Pages/Signup.jsx";
+import Login from "./Pages/Login.jsx";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
 
